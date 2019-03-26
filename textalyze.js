0bf1ce9327e8f40e82deba5d64adadfa575f7f2d
joody <joody@hubify.com.br>
2019-03-26 15:09:42 -0300

    Added require.main === module condition for manual testing
    In order to prevent the manual testing running when imported as application modules, a condition was added. Now it only runs when execu
    ted directly.
    
    For execute with manual testing run on terminal:
    
    - node textalyze.js
    or
    - npm start

--- textalyze.js
+++ textalyze.js
@@ -30,16 +30,18 @@ function itemCounts(array) {
   return counts;
 }
 
-let args = "";
+if (require.main === module) {
+  
+  let args = "aabbccd";
+  console.log(`The letter count for ${args} is: `);
+  console.log(itemCounts(args.split('')));
 
-args = "aabbccd";
-console.log(`The letter count for ${args} is: `);
-console.log(itemCounts(args.split('')));
+  console.log('--------------------------');
 
-console.log('--------------------------');
-
-args = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque blandit turpis, eu pellentesque dui convallis vitae. Maecenas elit sem, fermentum non pellentesque eu, lacinia eget dui. Fusce accumsan, ex eget facilisis bibendum, est tortor congue tortor, varius aliquam ex magna et metus. Praesent suscipit diam eu diam tincidunt egestas. Quisque maximus purus posuere nisi dapibus, eu ornare diam fermentum. Vestibulum bibendum, mauris condimentum feugiat consectetur, nisl nunc aliquet urna, vel auctor magna diam sed mauris. Morbi faucibus, ex in hendrerit egestas, nulla purus gravida tortor, ac bibendum ligula dolor vel diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris facilisis, dolor posuere mattis ultrices, felis nibh pulvinar metus, at fringilla dolor ante eu augue. Ut auctor lacus ac odio venenatis pretium. Fusce eleifend lorem eu tincidunt pulvinar. Donec eu tellus et justo pharetra bibendum ut id libero. Fusce luctus velit ultricies ante iaculis fringilla.";
-console.log(`The letter count for ${args} is: `);
-console.log(itemCounts(args.split('')));
+  args = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque blandit turpis, eu pellentesque dui convallis vitae. Maecenas elit sem, fermentum non pellentesque eu, lacinia eget dui. Fusce accumsan, ex eget facilisis bibendum, est tortor congue tortor, varius aliquam ex magna et metus. Praesent suscipit diam eu diam tincidunt egestas. Quisque maximus purus posuere nisi dapibus, eu ornare diam fermentum. Vestibulum bibendum, mauris condimentum feugiat consectetur, nisl nunc aliquet urna, vel auctor magna diam sed mauris. Morbi faucibus, ex in hendrerit egestas, nulla purus gravida tortor, ac bibendum ligula dolor vel diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris facilisis, dolor posuere mattis ultrices, felis nibh pulvinar metus, at fringilla dolor ante eu augue. Ut auctor lacus ac odio venenatis pretium. Fusce eleifend lorem eu tincidunt pulvinar. Donec eu tellus et justo pharetra bibendum ut id libero. Fusce luctus velit ultricies ante iaculis fringilla.";
+  console.log(`The letter count for ${args} is: `);
+  console.log(itemCounts(args.split('')));
+ 
+}
 
 module.exports = { itemCounts };
